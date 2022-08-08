4825e4dcce4482af4330c9b297e89b976f677d0b
Federico Giorgino <federicog.dev@gmail.com>
2022-08-08 14:07:31 +0200

    AppStack setup

---
+++ src/navigation/AppStack.tsx
@@ -0,0 +1,40 @@
+import React, { useContext } from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { AppStackParams } from "../types/navigation";
+import {
+  AdminScreen,
+  BookmarksScreen,
+  HomeScreen,
+  LoginScreen,
+  PostDetailsScreen,
+  RegisterScreen,
+  SearchScreen,
+} from "../screens";
+
+const Stack = createNativeStackNavigator<AppStackParams>();
+
+export const AppStack = () => {
+  const { Navigator, Screen, Group } = Stack;
+  const token = false;
+
+  const admin = false;
+
+  return (
+    <Navigator>
+      {!token ? (
+        <Group screenOptions={{ headerShown: false }}>
+          <Screen name="LoginScreen" component={LoginScreen} />
+          <Screen name="RegisterScreen" component={RegisterScreen} />
+        </Group>
+      ) : (
+        <Group>
+          <Screen name="HomeScreen" component={HomeScreen} />
+          {admin && <Screen name="AdminScreen" component={AdminScreen} />}
+          <Screen name="BookmarksScreen" component={BookmarksScreen} />
+          <Screen name="PostDetailsScreen" component={PostDetailsScreen} />
+          <Screen name="SearchScreen" component={SearchScreen} />
+        </Group>
+      )}
+    </Navigator>
+  );
+};
---
+++ src/types/navigation.ts
@@ -0,0 +1,10 @@
+export type AppStackParams = {
+  LoginScreen: undefined;
+  RegisterScreen: undefined;
+  HomeScreen: undefined;
+  AdminScreen: undefined;
+  ProfileScreen: { id: string };
+  BookmarksScreen: undefined;
+  PostDetailsScreen: { id: string };
+  SearchScreen: undefined;
+};
